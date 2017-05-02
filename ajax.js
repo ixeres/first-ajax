13d15095f1a72cdada32aa2e2c23aa8bab03308e
Devin Leduc <ixeres@gmail.com>
2017-05-02 12:41:12 -0400

    Finished up to step 8. Am now a Time Wizard.

--- ajax.js
+++ ajax.js
@@ -1,5 +1,74 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+  var rootURL = 'http://first-ajax-api.herokuapp.com/';
+
+  $('#step1').on('click', function() {
+    $.ajax({
+      url: rootURL,
+      method: 'GET',
+      data: '{}',
+      dataType: 'JSON',
+    });
+  });
+
+  var runNum = 0;
+function pingPong() {
+  if ((runNum % 2) === 0) {
+    runNum++;
+    return 'ping';
+  } else {
+    runNum++;
+    return 'pong';
+  }
+}
+
+$('#step3').on('click', function() {
+  $.ajax({
+  url: rootURL + pingPong(),
+  method: 'GET',
+  dataType: 'text'
+}).done(function (responseData) {
+  console.log((responseData));
+  $('#step3456').append((responseData));
+}).fail(function(responseData) {
+  console.log('FAIL. You dun goofed!');
+$('#step3456').append('FAIL. You dun goofed!');
+}).always(function() {
+  console.log('Some stuff happened. Now it is done.')
+});
+});
+
+$('#seven').on('click', function() {
+  $.ajax({
+  url: rootURL + '/count',
+  method: 'GET',
+  dataType: 'text'
+}).done(function (responseData) {
+  console.log((responseData));
+  $('#seven').append((responseData));
+}).fail(function(responseData) {
+  console.log('FAIL. You dun goofed!');
+$('#seven').append('FAIL. You dun goofed!');
+}).always(function() {
+  console.log('Some stuff happened. Now it is done.')
+});
+});
+
+$('#eight').on('click', function() {
+  $.ajax({
+  url: rootURL + '/time',
+  method: 'GET',
+  data: { timezone: 'Asia/Kolkata' },
+  dataType: 'text'
+}).done(function (responseData) {
+  console.log((responseData));
+  $('#eight').append((responseData));
+}).fail(function(responseData) {
+  console.log('FAIL. You dun goofed!');
+$('#eight').append('FAIL. You dun goofed!');
+}).always(function() {
+  console.log('Some stuff happened. Now it is done.')
+});
+});
 
 });
